ae648a17bb92a67d813303498bbba6ca370a675e
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-05 16:20:22 +0200

    commit aggiunta anno

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,7 @@ const userSurname = prompt("Qual è il tuo cognome?")
 console.log(userSurname); 
 const userColor = prompt("Qual è il tuo colore preferito?")
 console.log(userColor); 
-const newPassword = ('Ecco la tua nuova Password: ' + userName + userSurname + userColor)
+const currentYear = 22
+console.log(currentYear);
+const newPassword = ('Ecco la tua nuova Password: ' + userName + userSurname + userColor + currentYear)
 document.getElementById('password').innerHTML = newPassword
